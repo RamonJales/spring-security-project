7215c06a5b9527af0e71245fe3c4216cefa4d9db
RamonJales <ramonjales123@gmail.com>
2022-03-17 17:13:59 -0300

    Adding admin person

--- src/main/java/br/com/config/SecurityConfiguration.java
+++ src/main/java/br/com/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration // Tells spring that this class is configuration
 @EnableWebSecurity // Tells spring that this class not only contains configurations, it also contains configurations for web
@@ -19,13 +20,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+		http.authorizeRequests()
+			.antMatchers("/index").access("hasAnyAuthority('USERS', 'ADMIN')")
+			.antMatchers("/private-page").access("hasAuthority('ADMIN')")
+			.anyRequest().authenticated().and().formLogin()
+			.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/login").permitAll();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("ramon")
-			.password(passwordEncoder().encode("123")).authorities("USER");
+		auth.inMemoryAuthentication()
+			.withUser("ramon")
+			.password(passwordEncoder().encode("123"))
+			.authorities("ADMIN")
+			.and()
+			.withUser("maria")
+			.password(passwordEncoder().encode("456"))
+			.authorities("USER");
 	}
 	
 	
--- src/main/java/br/com/controllers/HomeController.java
+++ src/main/java/br/com/controllers/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String login() {
 		return "login";
+	}
+	
+	@GetMapping("/private-page")
+	public String privatePage() {
+		return "private-page";
 	}
 }
